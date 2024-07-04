95fb577b5c2c56efbca3cb06d25b49cc64da0d9f
DeniFarm <deni.farmasi.103040@gmail.com>
2024-07-04 23:02:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/yt", (req, res) => {
-  const searchQuery = req.query.s;
-  if (!searchQuery) {
-    return res.send('Please provide a search query using the "s" query parameter.');
+app.get("/yt", async (req, res) => {
+  const query = req.query.s;
+  if (!query) {
+    return res.status(400).send("Query parameter 's' is required");
   }
-  scrapeLogic(res, searchQuery);
+  await scrapeLogic(query, res);
 });
 
 app.get("/", (req, res) => {
