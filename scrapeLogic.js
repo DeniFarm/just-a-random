25c3af609988fc01b3b245345fb7c6975c48e0ac
DeniFarm <deni.farmasi.103040@gmail.com>
2024-07-04 23:01:50 +0530

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,8 +1,7 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
-const path = require("path");
 
-const scrapeLogic = async (res, searchQuery) => {
+const scrapeLogic = async (query, res) => {
   const browser = await puppeteer.launch({
     args: [
       "--disable-setuid-sandbox",
@@ -15,14 +14,15 @@ const scrapeLogic = async (res, searchQuery) => {
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
+
   try {
     const page = await browser.newPage();
-    await page.goto(`https://www.youtube.com/results?search_query=${encodeURIComponent(searchQuery)}`);
-    await page.waitForSelector('ytd-thumbnail');
+    await page.goto(`https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`);
+    await page.waitForSelector('ytd-video-renderer', { timeout: 10000 });
 
-    const screenshotPath = path.join(__dirname, 'screenshot.png');
-    await page.screenshot({ path: screenshotPath });
-    res.sendFile(screenshotPath);
+    const screenshotBuffer = await page.screenshot();
+    res.set('Content-Type', 'image/png');
+    res.send(screenshotBuffer);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
